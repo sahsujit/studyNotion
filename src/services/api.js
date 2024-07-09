afa73a26474e1f315cc11ff4877a8571ff76b03d
꧁༺քʀօɢʀǟʍʍɛʀ ֆʊʝɨȶ༻꧂ <129708170+sahsujit@users.noreply.github.com>
2024-07-09 15:28:38 +0545

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:4000/api/v1"
+const BASE_URL = "https://studynotion-server-vdjq.onrender.com/api/v1"
 // const BASE_URL ="https://studynotion-server-two.vercel.app/"
 
 // AUTH ENDPOINTS
